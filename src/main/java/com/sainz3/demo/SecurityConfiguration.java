b0219ba136112f699cbc05ebe69627478d5ec3c6
danielasainz <dasainz@email.wm.edu>
2018-03-22 16:23:02 -0400

    fixed functionalities

--- src/main/java/com/sainz3/demo/SecurityConfiguration.java
+++ src/main/java/com/sainz3/demo/SecurityConfiguration.java
@@ -2,25 +2,28 @@ package com.sainz3.demo;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure (HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .and().httpBasic();
+                .and()
+                .httpBasic();
+    }
 
         @Override
-        protected void configure(AuthenticationManagerBuilder auth)
+        protected void configure (AuthenticationManagerBuilder auth)
             throws Exception {
 
             auth.inMemoryAuthentication().
@@ -28,4 +31,3 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         }
     }
 
-}
